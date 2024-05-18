ddf78638069bfb29830dbdd18b7283a4f8b8717b
utku-guclu <utku_guclu@hotmail.com>
2024-05-19 00:54:42 +0300

    hover - shadow

--- app/nasa/apod/slide/page.tsx
+++ app/nasa/apod/slide/page.tsx
@@ -48,12 +48,14 @@ export default function ImageDetail({ params }: Props) {
         priority={true}
       />
       <div className="absolute inset-0 flex items-end justify-center max-h-fit">
-        <div className="relative group overflow-y-auto h-full">
-          <div className="bg-black bg-opacity-50 p-4 text-white opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
-            <h1 className="text-6xl">{data.title}</h1>
-            <hr />
-            <br />
-            <p className="text-lg">{data.explanation}</p>
+        <div className="relative group">
+          <div
+            style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.75)" }}
+            className="bg-black bg-opacity-50 p-4 text-white opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 overflow-y-auto max-h-full w-full md:p-6"
+          >
+            <h1 className="text-4xl md:text-6xl">{data.title}</h1>
+            <hr className="my-2" />
+            <p className="text-base md:text-lg">{data.explanation}</p>
           </div>
         </div>
       </div>
