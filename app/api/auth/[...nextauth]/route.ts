b7fcfa363b127932b5655eab30a6575610d3e4ae
utku-guclu <utku_guclu@hotmail.com>
2024-05-20 12:30:19 +0300

    local settings

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,14 @@
-import { authOptions } from "@/utils/authOptions";
-import NextAuth from "next-auth/next";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
 
 const handler = NextAuth(authOptions);
 
